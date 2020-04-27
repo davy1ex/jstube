dac6aeae00da695be4246c2d04298608bf4f2f56
davy1ex <davy1ex@gmail.com>
2020-04-27 17:18:15 +0400

    adding design (lol)

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+const path = require('path');
 const express = require("express");
 const app = express();
+const pug = require('pug');
+const favicon = require('serve-favicon');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public',  'img', 'favicon.png')));
+
+
+
 
 app.get("/", function(request, response) {
-    response.send("жопа")
+    response.render('index', {title: 'JSTUBE | Index Page'})
 });
 
 app.listen(3000);
